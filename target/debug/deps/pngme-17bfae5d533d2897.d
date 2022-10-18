527e26aa499651af3d1cad25bf6d7f2d195b3a27
g0sk <g0sk@pop-os.localdomain>
2022-10-18 12:35:49 +0100

    Initial commit

---
+++ target/debug/deps/pngme-17bfae5d533d2897.d
@@ -0,0 +1,5 @@
+/home/g0sk/Projects/pngme/target/debug/deps/pngme-17bfae5d533d2897.rmeta: src/main.rs
+
+/home/g0sk/Projects/pngme/target/debug/deps/pngme-17bfae5d533d2897.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/pngme-68b18b3dc61f495b.d
@@ -0,0 +1,5 @@
+/home/g0sk/Projects/pngme/target/debug/deps/pngme-68b18b3dc61f495b.rmeta: src/main.rs
+
+/home/g0sk/Projects/pngme/target/debug/deps/pngme-68b18b3dc61f495b.d: src/main.rs
+
+src/main.rs:
